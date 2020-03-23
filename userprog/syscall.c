197ac9ad7e995648906babcfd049bf1df4efd28f
fernandosagastume <fernando.sagastume@galileo.edu>
2020-03-22 19:53:57 -0600

    Add syscall_create and syscall_remove

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,16 +1,23 @@
 #include "userprog/syscall.h"
 #include <stdio.h>
+#include <stdbool.h>
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 #include "devices/shutdown.h"
+#include "threads/synch.h"
+#include "filesys/filesys.h"
 
 static void syscall_handler (struct intr_frame *);
+
+//Se crea una variable tipo lock para el fylesystem
+struct lock lockFS;
 
 void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+  lock_init(&lockFS);
 }
 
 static void
@@ -27,4 +34,24 @@ syscall_handler (struct intr_frame *f UNUSED)
 void 
 syscall_exit (int exit_status UNUSED){
 	thread_exit();
+}
+
+bool 
+syscall_create(const char* file, unsigned initial_size){
+	//Si el nombre del archivo es incorrecto, se hace exit
+	if(file == NULL){
+		syscall_exit(-1);//exit_status not successful
+	}
+	lock_arquire(&lockFS);
+	bool success = filesys_create(file, initial_size);
+	lock_release(&lockFS);
+	return success;
+}
+
+bool 
+syscall_remove(const char* file){
+	lock_arquire(&lockFS);
+	bool success = filesys_create(file, initial_size);
+	lock_release(&lockFS);
+	return success;
 }
--- userprog/syscall.h
+++ userprog/syscall.h
@@ -1,7 +1,10 @@
+#include <stdbool.h>
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
 void syscall_init (void);
 void syscall_halt (void); 
 void syscall_exit (int exit_status);
+bool syscall_create(const char* file, unsigned initial_size);
+bool syscall_remove(const char* file);
 #endif /* userprog/syscall.h */
